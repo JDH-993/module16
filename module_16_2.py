779f528125659bde3a58be1df1173a1d7a3f7f16
JDH-993 <sonya.shlyakhova@mail.ru>
2024-12-30 17:23:14 +0700

    module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -13,9 +13,9 @@ async def read_item():
     return "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
-async def polz(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", examples="12")]):
+async def polz(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="12")]):
     return f'Вы вошли как пользователь № {user_id}'
 
 @app.get('/user/{username}/{age}')
-async def read_item(username: Annotated[str, Path(min_length=5, max_length=20,  description="Enter username", examples="Erturyn")], age: Annotated[int, Path(gt=18, le=120, description="Enter age", examples="20")]):
+async def read_item(username: Annotated[str, Path(min_length=5, max_length=20,  description="Enter username", example="Erturyn")], age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="20")]):
     return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
